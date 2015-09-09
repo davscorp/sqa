41ad8d95c3c94ce091f2f3e61f1762e70c77cf3b
DAVID LAWRENCE - 1206208523 <david.lawrence@bulan.cs.ui.ac.id>
2015-09-09 17:42:34 +0700

    hghgh

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>David Lawrence - 1206208523</title></html>')
+#    return HttpResponse('<html><title>David Lawrence - 1206208523</title></html>')
+     return render(request, 'index.html')
--- superlists/settings.py
+++ superlists/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'superlists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+               os.path.join(BASE_DIR, 'pmpl'),
+         ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
