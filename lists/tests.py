9536a339fdff8c7c1d570f76a1362d30f0ac1f90
DAVID LAWRENCE - 1206208523 <david.lawrence@bulan.cs.ui.ac.id>
2015-09-14 11:32:34 +0700

    Commit2

--- lists/tests.py
+++ lists/tests.py
@@ -12,8 +12,18 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()  #1
         response = home_page(request)  #2
-        expected_html = render_to_string('home.html')
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html',
+        {'new_item_text':  'A new list item'})
         self.assertEqual(response.content.decode(), expected_html)
         #self.assertTrue(response.content.startswith(b'<html>'))  #3
         #self.assertIn(b'<title>To-Do lists</title>', response.content)  #4
         #self.assertTrue(response.content.strip().endswith(b'</html>'))  #5
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
   #  return HttpResponse(b'<html><title>David Lawrence - 1206208523</title></html>')
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
